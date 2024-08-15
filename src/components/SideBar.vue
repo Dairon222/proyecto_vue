8a5be6e18cee308d22d9b3ddd1947b668b2f6d6b
Dairon <andresitof360@gmail.com>
2024-08-14 21:46:35 -0500

    Views and Router in vue

---
+++ src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra lateral</h2>
+        <ul>
+            <li>
+                <RouterLink to="/Contador">Contador</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/ListaDeTareas">Lista De Tareas</RouterLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup></script>
+
+<style>
+.sidebar {
+    width: 200px;
+    background-color: antiquewhite;
+    padding: 20px;
+}
+</style>
